11cb8952238a33586911d555ce5274eb0d275d03
vahomaha <123481505+vahomaha@users.noreply.github.com>
2023-02-21 08:42:48 -0500

    Update JavaApplication16.java

--- src/javaapplication16/JavaApplication16.java
+++ src/javaapplication16/JavaApplication16.java
@@ -15,6 +15,7 @@ public class JavaApplication16 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Welcome to Java");
     }
     
 }
